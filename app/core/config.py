3b8b269c0389b28155177097b84242bb5196cf24
Danil <noskovde2015@gmail.com>
2025-11-13 15:18:18 +0300

    init alembic

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,6 @@
+from pathlib import Path
 
-from pydantic import BaseModel, PostgresDsn
+from pydantic import BaseModel, AnyUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -16,7 +17,7 @@ class ApiPrefix(BaseModel):
 
 
 class DataBaseConfig(BaseModel):
-    url: PostgresDsn
+    url: AnyUrl
     echo: bool = False
     echo_pool: bool = False
     max_overflow: int = 10
@@ -24,8 +25,16 @@ class DataBaseConfig(BaseModel):
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=Path(__file__).parent.parent / ".env",
+        case_sensitive=False,
+        env_file_encoding="utf-8",
+        env_prefix="TEST__",
+        env_nested_delimiter="__"
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DataBaseConfig
 
 
 settings = Settings()

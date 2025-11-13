d6bad93333db97263040086b208573ac969e5e50
Danil <noskovde2015@gmail.com>
2025-11-13 15:39:44 +0300

    Create User shema

---
+++ app/core/shemas/UserShema.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, EmailStr, ConfigDict, Field, field_validator
+
+
+class UserBase(BaseModel):
+    """Base User shema"""
+    name: str = Field(min_length=2, max_length=20, description="User firs name")
+    surname: str = Field(min_length=2, max_length=20, description="User surname")
+    age: int = Field(ge=0, le=150, description="User age")
+    email: EmailStr
+
+    @field_validator("age", mode="before")
+    def age_validator(cls, v):
+        if v < 0:
+            raise ValueError("age can not be negative")
+        return v
+
+
+class UserCreate(UserBase):
+    """Shema for creating a new user"""
+    pass
+
+
+class UserRead(UserBase):
+    """Shema for reading a user"""
+    model_config = ConfigDict(
+        from_attributes=True
+    )
+    id: int
